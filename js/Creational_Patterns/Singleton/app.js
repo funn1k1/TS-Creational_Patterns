4ea6625f994ce773ec4ee09494e3fc6ff357a7f2
Eugene <yukovich01@mail.ru>
2022-07-12 02:54:39 +0300

    add singleton

---
+++ js/Creational_Patterns/Singleton/app.js
@@ -0,0 +1,31 @@
+"use strict";
+class MyMap {
+    constructor() {
+        this.map = new Map();
+    }
+    clean() {
+        this.map = new Map();
+    }
+    static get() {
+        if (!MyMap.instance) {
+            MyMap.instance = new MyMap();
+        }
+        return MyMap.instance;
+    }
+}
+class Service1 {
+    addMap(key, value) {
+        const myMap = MyMap.get();
+        myMap.map.set(key, value);
+    }
+}
+class Service2 {
+    getKey(key) {
+        const myMap = MyMap.get();
+        console.log(myMap.map.get(key));
+        myMap.clean();
+        console.log(myMap.map.get(key));
+    }
+}
+new Service1().addMap(1, 'Работает!');
+new Service2().getKey(1);
---
+++ typescript/Creational_Patterns/Singleton/app.ts
@@ -0,0 +1,37 @@
+class MyMap {
+  private static instance: MyMap;
+
+  map: Map<number,  string> = new Map();
+  private constructor() {} 
+
+  clean() {
+    this.map = new Map();
+  }
+
+  public static get(): MyMap {
+    if (!MyMap.instance) {
+      MyMap.instance = new MyMap();
+    }
+    
+    return MyMap.instance
+  }
+}
+
+class Service1 {
+  addMap(key: number, value: string) {
+    const myMap = MyMap.get();
+    myMap.map.set(key, value);
+  }
+}
+
+class Service2 {
+  getKey(key: number) {
+    const myMap = MyMap.get();
+    console.log(myMap.map.get(key))
+    myMap.clean();
+    console.log(myMap.map.get(key));
+  }
+}
+
+new Service1().addMap(1, 'Работает!');
+new Service2().getKey(1);
