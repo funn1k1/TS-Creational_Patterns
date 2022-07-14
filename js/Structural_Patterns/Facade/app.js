8af5e3dffd7501d418159ab62646e5946ec766e5
Eugene <yukovich01@mail.ru>
2022-07-14 21:25:14 +0300

    add facade

---
+++ js/Structural_Patterns/Facade/app.js
@@ -0,0 +1,39 @@
+"use strict";
+class Notify {
+    send(to, template) {
+        console.log(`Отправляю ${template} to: ${to}`);
+    }
+}
+class Log {
+    log(message) {
+        console.log(message);
+    }
+}
+class Template {
+    constructor() {
+        this.templates = [
+            { name: 'other', template: '<h1>Шаблон!</h1>' }
+        ];
+    }
+    getByName(name) {
+        return this.templates.find(t => t.name == name);
+    }
+}
+class NotificationFacade {
+    constructor() {
+        this.notify = new Notify();
+        this.logger = new Log();
+        this.template = new Template();
+    }
+    send(to, templateName) {
+        const data = this.template.getByName(templateName);
+        if (!data) {
+            this.logger.log('Не найден шаблон');
+            return;
+        }
+        this.notify.send(to, data.template);
+        this.logger.log('Шаблон отправлен');
+    }
+}
+const nf = new NotificationFacade();
+nf.send('admin@admin.com', 'other');
---
+++ typescript/Structural_Patterns/Facade/app.ts
@@ -0,0 +1,46 @@
+class Notify {
+  send(to: string, template: string) {
+    console.log(`Отправляю ${template} to: ${to}`);
+  }
+}
+
+class Log {
+  log(message: string) {
+    console.log(message);
+  }
+}
+
+class Template {
+  private templates = [
+    { name: 'other', template: '<h1>Шаблон!</h1>' }
+  ];
+
+  getByName(name: string) {
+    return this.templates.find(t => t.name == name);
+  }
+}
+
+class NotificationFacade {
+  private notify: Notify;
+  private logger: Log;
+  private template: Template;
+
+  constructor() {
+    this.notify = new Notify();
+    this.logger = new Log();
+    this.template = new Template();
+  }
+
+  send(to: string, templateName: string) {
+    const data = this.template.getByName(templateName);
+    if (!data) {
+      this.logger.log('Не найден шаблон');
+      return;
+    }
+    this.notify.send(to, data.template);
+    this.logger.log('Шаблон отправлен');
+  }
+}
+
+const nf = new NotificationFacade();
+nf.send('admin@admin.com', 'other');
